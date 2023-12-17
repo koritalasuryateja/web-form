a9c0e981a6b88043ec772d4f2a118dda9b4d5478
koritalasuryateja <151409464+koritalasuryateja@users.noreply.github.com>
2023-12-17 16:46:03 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
-#!/bin/sh
+#!/bin/bash
+pip install Flask
 
-# installing dependencies
-pip3 install threading
-pip3 install flask
-pip3 install secret
-pip3 install uuid
+npm install newman
+
+echo 'Setup completed.'
