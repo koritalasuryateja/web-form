b5a4f4e72fde48f9acfa4ff0e8f2f612572ebebd
koritalasuryateja <151409464+koritalasuryateja@users.noreply.github.com>
2023-12-17 15:47:51 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,40 +1,13 @@
+# Testing for Baseline flow and Extensions
 #!/bin/sh
 
-# Source NVM in the current shell
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+set -e # exit immediately if newman complains
+trap 'kill $PID' EXIT # kill the server on exit
 
-# Use the correct Node.js version with NVM
-nvm use 16
+chmod +x run.sh
 
-# Set the Flask app environment variable
-export FLASK_APP=server.py  # Replace with your actual Flask app filename
+bash run.sh &
+PID=$! # record the PID
 
-# Exit immediately if any command fails
-set -e
-
-# Start the Flask server in the background
-flask run &
-#PID=$!  # Record the PID of the Flask server
-
-python3 server.py &
-APP_PID=$!
-python3 test.py
-
-# Allow some time for the server to start
-sleep 5
-
-# Run Newman tests
-echo "Running Newman tests for multiple posts..."
-newman run forum_multiple_posts.postman_collection.json -e env.json
-
-echo "Running Newman tests for post read and delete..."
-newman run forum_post_read_delete.postman_collection.json -n 50
-
-# Kill the Flask server process
-kill $APP_PID
-
-echo "Tests completed."
-
-# Exit
-exit 0
+newman run forum_multiple_posts.postman_collection.json -e env.json # use the env file
+newman run forum_post_read_delete.postman_collection.json -n 50 # 50 iterations
