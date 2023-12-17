388d984fa6dcbeaba9a6e51f38ca87fc8b9ca35f
koritalasuryateja <151409464+koritalasuryateja@users.noreply.github.com>
2023-12-17 18:45:59 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,33 @@
 #!/bin/bash
-pip install Flask
 
-npm install newman
+pip3 install Flask flask-sqlalchemy flask-login flask-limiter marshmallow werkzeug
 
-echo 'Setup completed.'
+# Install or update NVM
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+# Source NVM in the current shell
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  
+
+# Install Node.js 16.x
+nvm install 16
+
+# Use Node.js 16.x
+nvm use 16
+
+# Install Newman
+npm install -g newman
+
+# Install OpenSSL
+pip3 install pyopenssl
+
+
+# Install datetime
+pip3 install datetime
+
+# Install secrets
+pip3 install secrets
+
+
+# Update pip and setup tools
+pip3 install --upgrade pip setuptools
