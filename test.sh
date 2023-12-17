40e301dbb69a7ecb2ff1fb673a3247f7c54570ff
koritalasuryateja <151409464+koritalasuryateja@users.noreply.github.com>
2023-12-17 17:36:16 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,9 +1,24 @@
-#!/bin/bash
-python3 app.py &
-APP_PID=$!
-python3 test.py
-kill -9 $APP_PID  
+#!/bin/sh
 
-# Run Postman collections with Newman
-newman run forum_multiple_posts.postman_collection.json -e env.json
-newman run forum_post_read_delete.postman_collection.json -e env.json
+# Source NVM in the current shell
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  
+
+set -e # exit immediately if newman complains
+trap 'kill $PID' EXIT # kill the server on exit
+
+./run.sh &
+PID=$! # record the PID
+
+# Use the correct Node.js version with NVM
+nvm use 16
+
+# Run Newman tests
+newman run forum_multiple_posts.postman_collection.json -e env.json # use the env file
+newman run forum_post_read_delete.postman_collection.json -n 50 # 50 iterations
+
+# Additional test commands (if needed)
+# ...
+
+# Exit
+exit 0
