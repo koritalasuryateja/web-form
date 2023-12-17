742d3a290f5be03a136de467b81a59ca863bfe1f
koritalasuryateja <151409464+koritalasuryateja@users.noreply.github.com>
2023-12-17 17:39:10 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-# Source NVM in the current shell
+# Install Node.js (modify this line to specify the desired Node.js version)
+NODE_VERSION="16.13.0"
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm install $NODE_VERSION
 
 set -e # exit immediately if newman complains
 trap 'kill $PID' EXIT # kill the server on exit
@@ -10,10 +13,8 @@ trap 'kill $PID' EXIT # kill the server on exit
 ./run.sh &
 PID=$! # record the PID
 
-# Use the correct Node.js version with NVM
-nvm use 16
-
 # Run Newman tests
+nvm use $NODE_VERSION
 newman run forum_multiple_posts.postman_collection.json -e env.json # use the env file
 newman run forum_post_read_delete.postman_collection.json -n 50 # 50 iterations
 
