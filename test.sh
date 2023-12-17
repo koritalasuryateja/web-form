a2242d4736d2a33049e00efe30afba8e8167107d
koritalasuryateja <151409464+koritalasuryateja@users.noreply.github.com>
2023-12-17 17:17:08 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,9 +2,8 @@
 python3 app.py &
 APP_PID=$!
 python3 test.py
+kill -9 $APP_PID  
 
 # Run Postman collections with Newman
 newman run forum_multiple_posts.postman_collection.json -e env.json
 newman run forum_post_read_delete.postman_collection.json -e env.json
-
-kill APP_PID=$!
