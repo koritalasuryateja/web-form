ae3a9e585316668b9057fd07adad4ebaa8d6f091
koritalasuryateja <151409464+koritalasuryateja@users.noreply.github.com>
2023-12-17 16:45:35 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
+# Activate the virtual environment
+source venv/bin/activate
+
+# Start the Flask server
+export FLASK_APP=app.py
+export FLASK_ENV=development
 exec flask run
