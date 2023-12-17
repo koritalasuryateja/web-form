9a48150d9eeb59f6afada2bbb703090d1c4941ee
koritalasuryateja <151409464+koritalasuryateja@users.noreply.github.com>
2023-12-17 18:29:18 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,89 +1,197 @@
+
+
 import unittest
 import json
-from app import app
-from users import UserManager
+from server import forum_app  # Import the Flask app from server.py
+from web_users import MemberManager, ForumMember  # Import the MemberManager and ForumMember from web_users.py
 
-class TestUserManager(unittest.TestCase):
-    """ Test suite for UserManager class. """
-
+class TestMemberManager(unittest.TestCase):
+    
+    member_manager = MemberManager()
+    # Placeholder for creating a new member and capturing valid_member_id and correct_user_key
+    # This should be replaced with actual logic to create a member and capture their ID and key
+    new_member = member_manager.register_member("new_user", "New User")
+    valid_member_id = new_member.member_id
+    correct_user_key = new_member.access_key
     def setUp(self):
-        """ Set up a UserManager instance for testing. """
-        self.user_manager = UserManager()
+            self.app = forum_app.test_client()  # Create a test client for the Flask app
+            self.member_manager = MemberManager()
 
     def test_user_creation(self):
-        """ Test creation of a regular user. """
-        user = self.user_manager.create_user("testuser", "Test User")
-        self.assertIsNotNone(user)
-        self.assertEqual(user.username, "testuser")
+        # Test user creation
+        response = self.app.post('/member', json={'member_name': 'testuser', 'full_name': 'Test User'})
+        self.assertEqual(response.status_code, 200)
+        data = json.loads(response.data)
+        self.assertTrue(data['member_name'].startswith('testuser'))
 
     def test_moderator_creation(self):
-        """ Test creation of a moderator. """
-        moderator = self.user_manager.create_moderator("moduser", "Mod User")
-        self.assertIsNotNone(moderator)
-        self.assertEqual(moderator.username, "moduser")
-        self.assertTrue(moderator.is_moderator)
+        # Test moderator creation
+        response = self.app.post('/add_moderator',
+                                headers={'Master-Key': 'admin_key'},
+                                json={'member_name': 'newmod', 'full_name': 'New Mod'})
+        self.assertEqual(response.status_code, 201)
 
-    def test_user_validation(self):
-        """ Test user validation logic. """
-        user = self.user_manager.create_user("validuser", "Valid User")
-        self.assertTrue(self.user_manager.validate_user(user.user_id, user.key))
-        self.assertFalse(self.user_manager.validate_user(user.user_id, "incorrect_key"))
+    def test_user_auth(self):
+        # Test user authentication
+        valid_user = self.member_manager.register_member("authuser", "Auth User")
+        response = self.app.get(f'/member/{valid_user.member_id}')
+        self.assertEqual(response.status_code, 200)
+        data = json.loads(response.data)
+        self.assertEqual(data['member_name'], 'testuser')
 
-    def test_user_retrieval(self):
-        """ Test retrieval of a user. """
-        user = self.user_manager.create_user("existinguser", "Existing User")
-        self.assertEqual(self.user_manager.get_user(user.user_id), user)
-
-        non_existent_user = self.user_manager.get_user(9999)  # Assuming ID 9999 doesn't exist
-        self.assertIsNone(non_existent_user)
+        # Add more test cases for user authentication here
 
 class TestFlaskApi(unittest.TestCase):
-    """ Test suite for Flask API endpoints. """
-
     def setUp(self):
-        """ Set up a test client for Flask application. """
-        app.testing = True
-        self.client = app.test_client()
-        self.user_manager = UserManager()
+        forum_app.testing = True  # Use the forum_app from server.py
+        self.app = forum_app.test_client()
+        self.member_manager = MemberManager()
 
     def test_create_user_endpoint(self):
-        """ Test the endpoint for creating a user. """
-        response = self.client.post('/user', json={'username': 'testuser'})
+        # Test the create user endpoint
+        response = self.app.post('/member', json={'member_name': 'testuser', 'full_name': 'Test User'})
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json['username'], 'testuser')
+        data = json.loads(response.data)
+        self.assertEqual(data['member_name'], 'testuser')
 
     def test_create_moderator_endpoint(self):
-        """ Test the endpoint for creating a moderator. """
-        response = self.client.post('/create_moderator',
-                                    headers={'Admin-Key': 'admin_key'},
-                                    json={'username': 'newmod', 'real_name': 'New Mod'})
+        # Test the create moderator endpoint
+        response = self.app.post('/add_moderator',
+                                headers={'Master-Key': 'admin_key'},
+                                json={'member_name': 'newmod', 'full_name': 'New Mod'})
         self.assertEqual(response.status_code, 201)
 
     def test_post_creation_endpoint(self):
-        """ Test the endpoint for creating a post. """
-        response = self.client.post('/post', json={'msg': 'Test Post'})
-        self.assertEqual(response.status_code, 201)
-
-    def test_post_delete_endpoint(self):
-        """ Test the endpoint for deleting a post. """
-        # Create a post
-        create_response = self.client.post('/post', json={'msg': 'Test Post'})
-        self.assertEqual(create_response.status_code, 201)
-        self.assertTrue('id' in create_response.json)
-        self.assertTrue('key' in create_response.json)
-       
-        post_id = create_response.json['id']
-        key = create_response.json['key']
-       
-        # Delete the post
-        delete_response = self.client.delete(f'/post/{post_id}/delete/{key}')
-        self.assertEqual(delete_response.status_code, 200)
+        # Test post creation
+        post_data = {'message': 'This is a test post'}
+        response = self.app.post('/discussion', json=post_data)
+        self.assertEqual(response.status_code, 200)
 
     def test_post_read_endpoint(self):
-        """ Test the endpoint for reading a post. """
-        # Assuming a post with ID 1 exists
-        response = self.client.get('/post/1')
+        # Test reading a post
+        # Create a post then test reading that post
+        post_data = {'message': 'Test Post for Reading'}
+        create_response = self.app.post('/discussion', json=post_data)
+        post_id = create_response.json['id']
+        response = self.app.get(f'/discussion/{post_id}')
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_delete_endpoint(self):
+        # Create a post and get its ID and key
+        post_data = {'message': 'Test Post for Deletion'}
+        create_response = self.app.post('/discussion', json=post_data)
+        post_id = create_response.json['id']
+        key = create_response.json['key']
+
+        # Delete the post
+        delete_url = f'/discussion/{post_id}/remove/{key}'
+        response = self.app.delete(delete_url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_edit_endpoint(self):
+        # Create a post and get its ID and key
+        post_data = {'message': 'Test Post for Editing'}
+        create_response = self.app.post('/discussion', json=post_data)
+        post_id = create_response.json['id']
+        key = create_response.json['key']
+
+        # Edit the post
+        edit_data = {'post_id': post_id, 'member_key': key, 'new_message': 'Edited Test Post'}
+        edit_url = f'/discussion/{post_id}/edit'
+        response = self.app.put(edit_url, json=edit_data)
+        self.assertEqual(response.status_code, 200)
+
+    def test_filter_posts_by_date_endpoint(self):
+        # Test filtering posts by date
+        post_data = {'message': 'Test Post for Filtering by Date'}
+        create_response = self.app.post('/discussion', json=post_data)
+        post_id = create_response.json['id']
+        response = self.app.get(f'/discussions?year=2023')
+        self.assertEqual(response.status_code, 200)
+
+    def test_filter_posts_by_member_endpoint(self):
+        # Test filtering posts by member
+        post_data = {'message': 'Test Post for Filtering by Member'}
+        create_response = self.app.post('/discussion', json=post_data)
+        post_id = create_response.json['id']
+        response = self.app.get(f'/discussions/member/{1}')  # Assuming a member with ID 1 exists
+        self.assertEqual(response.status_code, 200)
+
+    def test_update_user_profile_endpoint(self):
+        # Test updating user profile
+        update_data = {'user_key': 'user_key', 'new_real_name': 'Updated User'}
+        response = self.app.put(f'/user/{1}/update', json=update_data)  # Assuming a user with ID 1 exists
+        self.assertEqual(response.status_code, 200)
+
+    def test_search_posts_endpoint(self):
+        # Test searching for posts
+        response = self.app.get('/posts/search?keyword=test')
         self.assertEqual(response.status_code, 200)
 
 if __name__ == '__main__':
     unittest.main()
+
+    # Placeholder test for is_ip_restricted_or_blocked
+    def test_isiprestrictedorblocked(self):
+        # TODO: Implement test logic for is_ip_restricted_or_blocked
+        pass
+
+    # Placeholder test for verify_ip_status
+    def test_verifyipstatus(self):
+        # TODO: Implement test logic for verify_ip_status
+        pass
+
+    # Placeholder test for get_member_info
+    def test_getmemberinfo(self):
+        # TODO: Implement test logic for get_member_info
+        pass
+
+    # Placeholder test for update_member_info
+    def test_updatememberinfo(self):
+        # TODO: Implement test logic for update_member_info
+        pass
+
+    # Placeholder test for add_post
+    def test_addpost(self):
+        # TODO: Implement test logic for add_post
+        pass
+
+    # Placeholder test for view_post
+    def test_viewpost(self):
+        # TODO: Implement test logic for view_post
+        pass
+
+    # Placeholder test for remove_post
+    def test_removepost(self):
+        # TODO: Implement test logic for remove_post
+        pass
+
+    # Placeholder test for edit_post
+    def test_editpost(self):
+        # TODO: Implement test logic for edit_post
+        pass
+
+    # Placeholder test for __init__
+    def test_init(self):
+        # TODO: Implement test logic for __init__
+        pass
+
+    # Placeholder test for __init__
+    def test_init(self):
+        # TODO: Implement test logic for __init__
+        pass
+
+    # Placeholder test for create_forum_moderator
+    def test_createforummoderator(self):
+        # TODO: Implement test logic for create_forum_moderator
+        pass
+
+    # Placeholder test for validate_member
+    def test_validatemember(self):
+        # TODO: Implement test logic for validate_member
+        pass
+
+    # Placeholder test for get_member_info
+    def test_getmemberinfo(self):
+        # TODO: Implement test logic for get_member_info
+        pass
